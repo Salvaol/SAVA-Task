58a56437f00b4e5e483c4af0fda36291b5a35cf3
salvaol <salvaollero.94@gmail.com>
2022-07-05 16:06:05 +0200

    test fixed

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,5 +1,6 @@
 import pytest
 import ETL.extract as E
+import os
 
 
 def test_extract_psfile_ok_without_path():
@@ -15,12 +16,9 @@ def test_extract_psfile_ok_without_path():
 def test_extract_psfile_ok_with_path():
     assert E is not None
 
-    measurements, curves = E.Extraction(path=r"C:\Users\devil\Downloads\SAVA-task", container='data'
-                                            , batch='batch1', filename='tc2025.pssession').extract_psfile()
+    measurements, curves = E.Extraction(path=os.path.dirname(os.path.dirname(__file__)), container='data'
+                                        , batch='batch1', filename='tc2025.pssession').extract_psfile()
 
     assert measurements is not None
     assert curves is not None
     assert isinstance(curves, list)
-
-
-
